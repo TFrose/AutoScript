4a79e348d33c4531a583370f2ce927c5037b9917
TFrose <tfrose12@163.com>
2022-06-27 17:06:33 +0800

    modify

--- use_pstuil.py
+++ use_pstuil.py
@@ -9,4 +9,16 @@
 # @desc : use pstuil and get data of the linux system
 
 import paramiko
-paramiko.SFTP
+
+# 建立一个sshclient对象
+ssh = paramiko.SSHClient()
+# 允许将信任的主机自动加入到host_allow 列表，此方法必须放在connect方法的前面
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# 调用connect方法连接服务器
+ssh.connect(hostname='114.116.79.140', port=22, username='root', password='Dans12a?')
+# 执行命令
+stdin, stdout, stderr = ssh.exec_command('df -hl')
+# 结果放到stdout中，如果有错误将放到stderr中
+print(stdout.read().decode())
+# 关闭连接
+ssh.close()
